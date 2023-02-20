7480570c18aef71ef8507f16a552841709be31e8
Ivan <vasiutochkin1@gmail.com>
2023-02-20 20:30:01 +0200

    gg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import {NavBar} from "./Components/NavBar";
+import {MoviesGenre, NavBar} from "./components";
+import {Route, Routes} from "react-router-dom";
+import React from "react";
+import {MovieDetailsPage, MoviesPage, NotFoundPage, TrendingPage, TvShowsPage} from "./pages";
 
 const App = () => {
     return (
         <div>
             <NavBar/>
+            <Routes>
+                <Route path='' element={<MoviesPage/>}/>
+                <Route path='/genre' element={<MoviesGenre/>}/>
+                <Route path='/tvShows' element={<TvShowsPage/>}/>
+                <Route path='/trending' element={<TrendingPage/>}/>
+
+                <Route path='/:movieId' element={<MovieDetailsPage/>}/>
+            </Routes>
+            {/*<Route path={'*'} element={<NotFoundPage/>}/>*/}
         </div>
     );
 };
---
+++ src/pages/moviesPage/MoviesPage.js
@@ -0,0 +1,11 @@
+import {Movies} from "../../components";
+
+const MoviesPage = () => {
+    return (
+        <div>
+            <Movies/>
+        </div>
+    );
+};
+
+export {MoviesPage};
